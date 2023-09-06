ee2ab6f18742eb2b5429d0ab23aa90bfc39326e3
Christian Martinez <christianandremartinez@gmail.com>
2023-09-05 21:13:25 -0600

    versionverdadera

--- interfaz.js
+++ interfaz.js
@@ -1,48 +1,47 @@
-const readline = require('readline');
 const tareas = require('./tareas');
-const funciones = require('./funciones')
-//================================================//
-  let rl = readline.createInterface({
-        input:process.stdin,
-        output:process.stdout
-  });
+const funciones = require('./funciones');
+const readlineSync = require('readline-sync');
 
-  console.log('Bienvenido a su lista de Tareas');
-  console.log('Que haremos hoy?');
+console.log('Bienvenido a su lista de Tareas');
+console.log('Que haremos hoy?');
 
-  function mostrarMenu() {  
-    console.log('\nMenú:');
-    console.log('1. Agregar nueva tarea');
-    console.log('2. Eliminar una tarea');
-    console.log('3. Completar tarea');
-    console.log('4. Revisar lista de tareas');
-    console.log('5. Salir');
-  
-    rl.question('Seleccione una opción: ', (opcion) => {
-      switch (opcion) {
-        case '1':
-            funciones.agregarTarea()
-            mostrarMenu();
-          break;
-        case '2':
-          funciones.eliminarTarea()
-          mostrarMenu();
-          break;
-        case '3':
-          funciones.marcarTareaComoCompletada()
-          mostrarMenu();
-          break;
-        case '4':
-          funciones.mostrarTareas()
-          mostrarMenu();
-          break;
-        case '5':
-          break;
-        default:
-          console.log('Opción no válida.');
-          mostrarMenu();
-      }
-    });
+function mostrarMenu() {
+  console.log('\nMenú:');
+  console.log('1. Agregar nueva tarea');
+  console.log('2. Eliminar una tarea');
+  console.log('3. Completar tarea');
+  console.log('4. Revisar lista de tareas');
+  console.log('5. Salir');
+
+  const opcion = readlineSync.question('Seleccione una opcion: ');
+
+  switch (opcion) {
+    case '1':
+      funciones.agregarTarea();
+      mostrarMenu();
+      break;
+    case '2':
+      funciones.eliminarTarea();
+      mostrarMenu();
+      break;
+    case '3':
+      funciones.marcarTareaComoCompletada();
+      mostrarMenu();
+      break;
+    case '4':
+      funciones.mostrarTareas();
+      mostrarMenu();
+      break;
+    case '5':
+      console.log('Saliendo del programa.');
+      process.exit();
+      break;
+    default:
+      console.log('Opción no válida.');
+      mostrarMenu();
   }
-  
-  mostrarMenu();  
+}
+
+mostrarMenu();
+
+module.exports = mostrarMenu()
